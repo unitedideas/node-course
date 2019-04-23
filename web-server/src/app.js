b12681bde94790d0e634f8aba81dd94db4cbaa1e
unitedideas <unitedideas@gmail.com>
2019-04-22 19:51:26 -0700

    dynamic pages using handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,9 +6,18 @@ console.log(path.join(__dirname, '../public'))
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
-
+app.set('view engine','hbs')
 app.use(express.static(publicDirectoryPath))
 
+app.get('',(req, res)=>{
+    res.render('index', {title: 'Weather', name: 'Shane'})
+})
+app.get('/about',(req, res)=>{
+    res.render('about', {title: 'About Me', name: 'Shane'})
+})
+app.get('/help',(req, res)=>{
+    res.render('help', {msg: '<p>created by Shane</p>'})
+})
 
 app.get('/weather', (req, res) => {
     res.send({location:'Phiily', temp: 48})
