531429b7780aed0a982cf652256c86d88997ebf9
unitedideas <unitedideas@gmail.com>
2019-04-23 12:14:44 -0700

    express views path setup

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,21 +6,35 @@ console.log(path.join(__dirname, '../public'))
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
-app.set('view engine','hbs')
+const viewsPath = path.join(__dirname, '../views')
+
+app.set('views', viewsPath)
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
-app.get('',(req, res)=>{
-    res.render('index', {title: 'Main Page', name: 'Shane'})
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Main Page',
+        name: 'Shane'
+    })
 })
-app.get('/about',(req, res)=>{
-    res.render('about', {title: 'About Me', name: 'Shane'})
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Shane'
+    })
 })
-app.get('/help',(req, res)=>{
-    res.render('help', {msg: 'This is some helpful text'})
+app.get('/help', (req, res) => {
+    res.render('help', {
+        msg: 'This is some helpful text'
+    })
 })
 
 app.get('/weather', (req, res) => {
-    res.send({location:'Phiily', temp: 48})
+    res.send({
+        location: 'Phiily',
+        temp: 48
+    })
 })
 
 const port = 5000
